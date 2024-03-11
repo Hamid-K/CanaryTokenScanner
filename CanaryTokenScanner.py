78eb40c087c628431ee5475cb3d9b3238574961f
Hamid <Hamid-K@users.noreply.github.com>
2024-03-11 15:54:06 +0100

    Update CanaryTokenScanner.py
    Improving output cosmetics and lowering false-positive.

--- CanaryTokenScanner.py
+++ CanaryTokenScanner.py
@@ -4,10 +4,13 @@ import re
 import shutil
 import sys
 import zlib
+from rich.console import Console
 from pathlib import Path
 
+console = Console()
+
 if len(sys.argv) != 2:
-    print("Usage: python script.py FILE_OR_DIRECTORY_PATH")
+    console.print("Usage: python script.py FILE_OR_DIRECTORY_PATH", style="bright_yellow")
     sys.exit(1)
 
 FILE_OR_DIRECTORY_PATH = sys.argv[1]
@@ -33,17 +36,23 @@ def process_pdf_file(pdf_path):
 
     return found_urls
 
+def check_domain(url):
+    allowed_domains = ['microsoft.com', 'w3.org', 'verisign.com', 'adobe.com', 'openxmlformats.org','purl.org']
+    for domain in allowed_domains:
+        if re.search(rf'https?://([a-zA-Z0-9.-]+\.)?{re.escape(domain)}', url):
+            return False
+    return True
+
 def decompress_and_scan(file_path):
-    is_suspicious = False
     temp_dir = "temp_extracted"
     os.makedirs(temp_dir, exist_ok=True)
+    is_suspicious = False
 
     try:
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
 
         url_pattern = re.compile(r'https?://\S+')
-        ignored_domains = ['schemas.openxmlformats.org', 'schemas.microsoft.com', 'purl.org', 'w3.org']
 
         for root, dirs, files in os.walk(temp_dir):
             for file_name in files:
@@ -52,12 +61,11 @@ def decompress_and_scan(file_path):
                     contents = extracted_file.read()
                     urls = url_pattern.findall(contents)
                     for url in urls:
-                        if not any(domain in url for domain in ignored_domains):
-                            print(f"URL Found in {file_path}:\n{url}")
+                        if check_domain(url):
+                            console.print(f"URL Found in [bold]{file_path}[/bold]: [red]{url}[/red]", style="bright_yellow")
                             is_suspicious = True
-
     except Exception as e:
-        print(f"Error processing file {file_path}: {e}")
+        console.print(f"Error processing file {file_path}: {e}", style="red")
 
     finally:
         shutil.rmtree(temp_dir, ignore_errors=True)
@@ -70,29 +78,26 @@ def is_suspicious_file(file_path):
     elif file_path.lower().endswith('.pdf'):
         urls = process_pdf_file(file_path)
         if urls:
-            print(f"The file {file_path} is suspicious. URLs found:")
-            for url in urls:
-                print(url.decode('utf-8', 'ignore').replace('/QXUGUTAENT)', ''))
-            return True
+            suspicious_urls = [url.decode('utf-8', 'ignore').replace('/QXUGUTAENT)', '') for url in urls if check_domain(url.decode('utf-8', 'ignore'))]
+            if suspicious_urls:
+                console.print(f"The file [bold]{file_path}[/bold] is suspicious. URLs found:", style="bright_yellow")
+                for url in suspicious_urls:
+                    console.print(f"[red]{url}[/red]", style="bright_yellow")
+                return True
     return False
 
 def main():
     if os.path.exists(FILE_OR_DIRECTORY_PATH):
         if os.path.isfile(FILE_OR_DIRECTORY_PATH):
             if is_suspicious_file(FILE_OR_DIRECTORY_PATH):
-                print(f"The file {FILE_OR_DIRECTORY_PATH} is suspicious.")
-            else:
-                print(f"The file {FILE_OR_DIRECTORY_PATH} seems normal.")
+                console.print(f"The file [bold]{FILE_OR_DIRECTORY_PATH}[/bold] is suspicious.", style="bright_yellow")
         elif os.path.isdir(FILE_OR_DIRECTORY_PATH):
             for root, dirs, files in os.walk(FILE_OR_DIRECTORY_PATH):
                 for file_name in files:
                     current_file_path = os.path.join(root, file_name)
-                    if is_suspicious_file(current_file_path):
-                        print(f"The file {current_file_path} is suspicious.")
-                    else:
-                        print(f"The file {current_file_path} seems normal.")
+                    is_suspicious_file(current_file_path)
     else:
-        print(f"The path {FILE_OR_DIRECTORY_PATH} does not exist.")
+        console.print(f"The path [bold]{FILE_OR_DIRECTORY_PATH}[/bold] does not exist.", style="bright_yellow")
 
 if __name__ == "__main__":
     main()
